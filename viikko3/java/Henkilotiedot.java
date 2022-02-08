be8d338814f564d6168844d14a8582cfb411cb7c
Emil Kantaneva <emil.kantaneva@gmail.com>
2022-02-08 16:59:29 +0200

    viikko 3 completed

--- viikko3/java/Henkilotiedot.java
+++ viikko3/java/Henkilotiedot.java
@@ -2,68 +2,63 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.imageio.plugins.tiff.ExifTIFFTagSet;
 
 
 public class Henkilotiedot {
-
     public static void printMenu(){
         System.out.println("1. Lisaa uusi henkilo");
-
         System.out.println("2. Poista henkilo");
-
         System.out.println("3. Tulosta henkilot");
-
         System.out.println("4. Lopeta");
     }
     public static void main ( String args[]){
 
-        // cout <=> System.out.print(ln)
-        // cin <=> Scanner ja System.in
-
-
-        /*
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Syota nimesi: ");
-        String nimi = scanner.nextLine();
-        System.out.println("Nimesi on " + nimi);
-        scanner.close();
-
-        Osoite pekan = new Osoite("Kissanmaankatu 20 D 191", "33530", "Tampere");
-
-        Henkilo pekka = new Henkilo("Pekka", 20, pekan);
-
-        pekka.tulostaTiedot();
-
+        Scanner scanner2 = new Scanner(System.in);
+        int valinta = 0;
+        
 
         List<Henkilo> henkilot = new ArrayList<Henkilo>();
-        henkilot.add(new Henkilo("Kalle", 20));
-        henkilot.add(new Henkilo("Maija", 23));
 
 
-        for (Henkilo h: henkilot ) {
-            h.tulostaTiedot();
-        }
-        */
-        Scanner scanner = new Scanner(System.in);
-        int valinta = scanner.nextInt();
-        List<Henkilo> henkilot = new ArrayList<Henkilo>();
 
         while (valinta != 4){
             printMenu();
-
+            valinta = scanner.nextInt();
             switch (valinta){
                 case 1:
                 //Toteutus
+                System.out.println("Syota nimi:");
+                String nimi = scanner2.nextLine();
+                System.out.println("Syota ika:");
+                int ika = Integer.parseInt(scanner2.nextLine());
+                System.out.println("Syota katu:");
+                String katu = scanner2.nextLine();
+                System.out.println("Syota postinumero:");
+                String postinro = scanner2.nextLine();
+                System.out.println("Syota kunta:");
+                String kunta = scanner2.nextLine(); 
+
+                henkilot.add(new Henkilo(nimi, ika, new Osoite(katu, postinro, kunta)));
+
                 break;
 
 
                 case 2:
-                //Toteutus
+                System.out.println("Syota poistettavan henkilon nimi:");
+                String poistettava = scanner2.nextLine();
+                henkilot.removeIf(h -> h.getNimi().contains(poistettava));
                 break;
 
+
                 case 3:
-                //toteutus
+                int index = 1;
+                for (Henkilo h: henkilot ) {
+                    System.out.println("Henkilo #" + index);
+                    h.tulostaTiedot();
+                    System.out.println("------------------------");
+                    index++;
+                }
                 break;
 
                 case 4:
@@ -73,20 +68,8 @@ public class Henkilotiedot {
                 System.out.println("Virheellinen valinta");
 
             }
-      
         }
-
-
-       
-
         scanner.close();
-
-        
-
-
-
-
+        scanner2.close();
     }
-
-
 }
